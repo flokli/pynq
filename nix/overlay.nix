2ba4e1a4b29b396b7b4776448a07b15e7f084ad4
Florian Klink <flokli@flokli.de>
2019-12-15 22:45:51 +0100

    nix: package ubootZynqPynqZ1
    This is using the nixpkgs-pinned uboot, with a custom patch on top of it
    adding a zynq_pynq_z1_defconfig and zynq-pynq-z1.dts meant for
    upstreaming into u-boot.
    
    It's not upstreamed yet, as it currently complains during boot:
    
    ```
    switch to partitions #0, OK
    mmc0 is current device
    Scanning mmc 0:1...
    Unknown command 'sf' - try 'help'
    SCRIPT FAILED: continuing...
    starting USB...
    Bus usb@e0002000: ULPI request timed out
    zynq ULPI viewport init failed
    probe failed, error -1
    No working controllers found
    USB is stopped. Please issue 'usb start' first.
    starting USB...
    Bus usb@e0002000: ULPI request timed out
    zynq ULPI viewport init failed
    probe failed, error -1
    No working controllers found
    ```
    
    It's still unclear whether these error messages are coming from the
    previous custom uboot env - at least the
    > Bus usb@e0002000: ULPI request timed out
    looks suspicious.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,4 +3,13 @@ self: super: {
 
   # https://github.com/NixOS/nixpkgs/pull/75722
   xilinx-bootgen = super.callPackage ./pkgs/xilinx-bootgen.nix {};
+
+  ubootZynqPynqZ1 = super.buildUBoot {
+    patches = [
+      ./pkgs/0001-ARM-zynq-add-Digilent-Zynq-PYNQ-Z1.patch
+    ];
+    defconfig = "zynq_pynq_z1_defconfig";
+    extraMeta.platforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot.elf"];
+  };
 }
