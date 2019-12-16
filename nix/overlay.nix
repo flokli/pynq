a4b89dae1f4d9ba26598f54d3a09e566cb8530b6
Florian Klink <flokli@flokli.de>
2019-12-16 19:35:03 +0100

    u-boot: rename patch commit message, shut up I2C and SPI bus dts warnings

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -6,7 +6,8 @@ self: super: {
 
   ubootZynqPynqZ1 = super.buildUBoot {
     patches = [
-      ./pkgs/0001-ARM-zynq-add-Digilent-Zynq-PYNQ-Z1.patch
+      ./pkgs/u-boot/0001-ARM-zynq-add-Digilent-Zynq-PYNQ-Z1.patch
+      ./pkgs/u-boot/0001-ARM-dts-xilinx-Fix-I2C-and-SPI-bus-warnings.patch
     ];
     defconfig = "zynq_pynq_z1_defconfig";
     extraMeta.platforms = ["armv7l-linux"];
