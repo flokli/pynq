3d27a8ae2cb5e5f6e90e9349cbdb34bdd0e43579
Florian Klink <flokli@flokli.de>
2019-12-17 21:18:12 +0100

    nix: bump nixpkgs
    xilinx-bootgen was upstreamed to nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  sha256 = "0bdsabk3g8asz3p43j1fqsxhm99q32j05p4lf2l59vvz1iswyiyw";
-  url = "https://github.com/NixOS/nixpkgs/archive/6cffb5072f9ad20d3ddaa54469d9b38f8720291c.tar.gz";
+  sha256 = "11vsxaq7hqflxpk3ib588zdrx39q4cvjaz0r19yvcqvmvcmjjnyx";
+  url = "https://github.com/NixOS/nixpkgs/archive/bc3fade573c8bfd49916b4afa3d00b37afe655ec.tar.gz";
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,8 +1,6 @@
 self: super: {
   openocd = super.callPackage ./pkgs/openocd.nix {};
 
-  # https://github.com/NixOS/nixpkgs/pull/75722
-  xilinx-bootgen = super.callPackage ./pkgs/xilinx-bootgen.nix {};
 
   pynq = {
     # everybody loves blobs, right?
