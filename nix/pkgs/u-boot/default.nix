c3ebf497481de3b2f77fccb589286ba7f4824abd
Florian Klink <flokli@flokli.de>
2020-01-04 02:05:17 +0100

    u-boot: base on current master
    This isn't necessary to get the board to boot with the SPL introduced in
    the previous commit, but still nice.

--- nix/pkgs/u-boot/default.nix
+++ nix/pkgs/u-boot/default.nix
@@ -1,5 +1,13 @@
-{ buildUBoot }:
+{ buildUBoot, fetchFromGitLab }:
 buildUBoot {
+  version = "v2020.01-rc5-73";
+  src = fetchFromGitLab {
+    domain = "gitlab.denx.de";
+    owner = "u-boot";
+    repo = "u-boot";
+    rev = "4b75aa5aa78768fc81b782ee51d960dfed76f6e1";
+    sha256 = "1s1i7jjx4g999b2h5dp7p3a5ahgg002iip7h0ah14761wx1xizv1";
+  };
   patches = [
     ./0001-ARM-zynq-add-Digilent-Zynq-PYNQ-Z1.patch
     ./0001-ARM-dts-xilinx-Fix-I2C-and-SPI-bus-warnings.patch
