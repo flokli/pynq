475b0186334d9199b2b32bbe3070920bdc334b44
Florian Klink <flokli@flokli.de>
2020-03-28 23:53:10 +0100

    nix: bump nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  sha256 = "11vsxaq7hqflxpk3ib588zdrx39q4cvjaz0r19yvcqvmvcmjjnyx";
-  url = "https://github.com/NixOS/nixpkgs/archive/bc3fade573c8bfd49916b4afa3d00b37afe655ec.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/f8ccef5edb3ffb02dc4d2ed46b3428aef0847f76.tar.gz";
+  sha256 = "07ppbzj0f18pvy22lp5pv2qq479c4yhkwq9v7vn19kpq22rg9vqb";
 }
