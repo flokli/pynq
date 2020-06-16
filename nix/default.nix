a584e062fc2d5ee947a265a5bab3c2a214bb745e
Florian Klink <flokli@flokli.de>
2020-06-16 21:06:01 +0200

    .envrc, nix: use pkgs.buildEnv to assemble the environment
    This gets rid of a lot of spam during environment entry, and also fixed
    `nix-shell` invocation from inside the environment.

--- nix/default.nix
+++ nix/default.nix
@@ -1,13 +1,27 @@
 let
   pkgs = import ./pkgs.nix;
+
+  profileEnv = pkgs.writeTextFile {
+    name = "profile-env";
+    destination = "/.profile";
+    # This gets sourced by direnv. Set NIX_PATH, so `nix-shell` uses the same nixpkgs as here.
+    text = ''
+      export NIX_PATH=nixpkgs=${toString pkgs.path}
+    '';
+  };
 in {
   inherit pkgs;
-  shell = pkgs.mkShell {
-    buildInputs = with pkgs; [
+
+  env = pkgs.buildEnv {
+    name = "dev-env";
+    paths = with pkgs; [
+      bluespec
       gdb
       openocd
       gnumake
       yosys
+
+      profileEnv
     ];
   };
 }
